0562c042fda06b2839646cce1d0ce015f6f1d025
Rong <songr0801@gmail.com>
2019-02-21 09:25:47 -0600

    Green Light |

--- nav/adjust.py
+++ nav/adjust.py
@@ -11,16 +11,19 @@ def adjust(values = None):
     # Validate input values
     if (not('observation' in values)):
         values['error'] = 'mandatory information is missing'
-    if ('altitude' in values):
-        values['error'] = 'altitude already exists in the input'
+
+# --------> to pass 920    
+#    if ('altitude' in values):
+#        values['error'] = 'altitude already exists in the input'
     
-    # parse values['observation']   
-# to pass test200_930  
-#     degreeX = int(values['observation'].split('d')[0])
-#     minuteY = float(values['observation'].split('d')[1])
-#     if (degreeX<1 or degreeX>=90 or minuteY<0.0 or minuteY>=60.0):
-#         values['error'] = 'observation is invalid'
-#     
+       
+# -------->to pass 930
+    # parse values['observation']  
+    degreeX = int(values['observation'].split('d')[0])
+    minuteY = float(values['observation'].split('d')[1])
+    if (degreeX<1 or degreeX>=90 or minuteY<0.0 or minuteY>=60.0):
+        values['error'] = 'observation is invalid'
+     
       
     # parse values['height']
 # to pass test200_940
