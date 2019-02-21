04e82e00ba5660a305e57f25466c73557881ed90
Rong <songr0801@gmail.com>
2019-02-21 03:08:17 -0600

    Red Light |

--- nav/adjust.py
+++ nav/adjust.py
@@ -11,7 +11,14 @@ def adjust(values = None):
 
     if (not('observation' in values)):
         values['error'] = 'mandatory information is missing'
-         
-
+    
+    
+    # function to parse values['observation']     
+    #  
     
     return values
+
+
+
+
+
