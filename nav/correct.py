d3397cec576ede0a8e17553800bd23ca32f5b248
Rong <songr0801@gmail.com>
2019-03-31 13:37:34 -0500

    Refactor |

--- nav/correct.py
+++ nav/correct.py
@@ -35,27 +35,32 @@ def correct(values = None):
 
 
     # validate 'lat' ---> extract method: isValidAngleStrFormat()
-    try:
-        # validate x: degree portion of the angle
-        degreeXstr = values['lat'].split('d')[0]
-        degreeX = int(degreeXstr)
-        
-        # validate y.y: minute portion of the angle
-        minuteYdotYstr = values['lat'].split('d')[1]
-        minuteYdotY = float(minuteYdotYstr)
-        if (minuteYdotY < 0.0 or minuteYdotY >= 60.0):
-            values['error'] = 'invalid lat'
-            return values        
-        
-        intPartYdotYstr = minuteYdotYstr.split('.')[0]        
-        fractPartYdotYstr = minuteYdotYstr.split('.')[1]
-        if (len(intPartYdotYstr) == 0 or len(fractPartYdotYstr) == 0):
-            values['error'] = 'invalid lat'
-            return values     
-    
-    except Exception:
+#     try:
+#         # validate x: degree portion of the angle
+#         degreeXstr = values['lat'].split('d')[0]
+#         degreeX = int(degreeXstr)
+#         
+#         # validate y.y: minute portion of the angle
+#         minuteYdotYstr = values['lat'].split('d')[1]
+#         minuteYdotY = float(minuteYdotYstr)
+#         if (minuteYdotY < 0.0 or minuteYdotY >= 60.0):
+#             values['error'] = 'invalid lat'
+#             return values        
+#         
+#         intPartYdotYstr = minuteYdotYstr.split('.')[0]        
+#         fractPartYdotYstr = minuteYdotYstr.split('.')[1]
+#         if (len(intPartYdotYstr) == 0 or len(fractPartYdotYstr) == 0):
+#             values['error'] = 'invalid lat'
+#             return values     
+#     
+#     except Exception:
+#         values['error'] = 'invalid lat'
+#         return values    
+    if (not isValidAngleStrFormat(values['lat'])):
         values['error'] = 'invalid lat'
-        return values    
+        return values
+
+
 
     # validate 'long'
 
