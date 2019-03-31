216dba4d76e63e000cb5bf2af18e4d082c6a1543
Rong <songr0801@gmail.com>
2019-03-31 13:49:03 -0500

    Green Light |

--- nav/correct.py
+++ nav/correct.py
@@ -34,35 +34,18 @@ def correct(values = None):
         return values
 
 
-    # validate 'lat' ---> extract method: isValidAngleStrFormat()
-#     try:
-#         # validate x: degree portion of the angle
-#         degreeXstr = values['lat'].split('d')[0]
-#         degreeX = int(degreeXstr)
-#         
-#         # validate y.y: minute portion of the angle
-#         minuteYdotYstr = values['lat'].split('d')[1]
-#         minuteYdotY = float(minuteYdotYstr)
-#         if (minuteYdotY < 0.0 or minuteYdotY >= 60.0):
-#             values['error'] = 'invalid lat'
-#             return values        
-#         
-#         intPartYdotYstr = minuteYdotYstr.split('.')[0]        
-#         fractPartYdotYstr = minuteYdotYstr.split('.')[1]
-#         if (len(intPartYdotYstr) == 0 or len(fractPartYdotYstr) == 0):
-#             values['error'] = 'invalid lat'
-#             return values     
-#     
-#     except Exception:
-#         values['error'] = 'invalid lat'
-#         return values    
+    # validate 'lat'   
     if (not isValidAngleStrFormat(values['lat'])):
         values['error'] = 'invalid lat'
         return values
-
+    
+    
 
 
     # validate 'long'
+    if (not isValidAngleStrFormat(values['long'])):
+        values['error'] = 'invalid long'
+        return values
 
 
 # ------------------------------------------------------
