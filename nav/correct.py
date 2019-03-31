7cea29d564d5ec8d47938146f857e70cdba0c2c7
Rong <songr0801@gmail.com>
2019-03-30 21:38:41 -0500

    Green Light |

--- nav/correct.py
+++ nav/correct.py
@@ -11,4 +11,39 @@
 
 
 def correct(values = None):
-    pass
+    
+    # validate mandatory element 'lat'
+    if (not('lat') in values):
+        values['error'] = 'mandatory information is missing'
+        return values
+    
+    # validate mandatory element 'long'
+    if (not('long') in values):
+        values['error'] = 'mandatory information is missing'
+        return values
+
+    # validate mandatory element 'altitude'
+    if (not('altitude') in values):
+        values['error'] = 'mandatory information is missing'
+        return values
+
+    # validate mandatory element 'assumedLat'
+    if (not('assumedLat') in values):
+        values['error'] = 'mandatory information is missing'
+        return values
+
+    # validate mandatory element 'assumedLong'
+    if (not('assumedLong') in values):
+        values['error'] = 'mandatory information is missing'
+        return values
+
+
+
+
+
+
+
+
+
+
+
