8b5cf36db691dc25647e023241a66c22c302acd6
Rong <songr0801@gmail.com>
2019-04-27 07:08:55 -0500

    Green Light |

--- nav/locate.py
+++ nav/locate.py
@@ -9,7 +9,19 @@
 
 
 def locate(values = None):
-    pass
+    
+    # check if mandatory elements exist in input values
+    if (not("assumedLat") in values):
+        values['error'] = 'mandatory information is missing'
+        return values
+    if (not("assumedLong") in values):
+        values['error'] = 'mandatory information is missing'
+        return values
+    if (not("corrections") in values):
+        values['error'] = 'mandatory information is missing'
+        return values
+    
+    
 
 
 
